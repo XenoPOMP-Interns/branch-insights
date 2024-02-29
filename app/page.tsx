bafdc764a31a5bf3373bf395a88685cb16a7f662
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-02-29 14:21:17 +0300

    🛞 put content to center

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import cn from 'classnames';
 import { Metadata } from 'next';
 
 import UiContainer from '@/src/components/ui/UiContainer/UiContainer';
 import { generateStaticMetadata } from '@/src/utils/seo';
-
-import styles from './main-page.module.scss';
+import cn from 'classnames';
 
 export const metadata: Metadata = generateStaticMetadata({
   title: 'Main',
@@ -12,8 +10,8 @@ export const metadata: Metadata = generateStaticMetadata({
 
 export default function Home() {
   return (
-    <main className={cn(styles.mainPage)}>
-      <UiContainer className={cn(styles.view)}>index page</UiContainer>
-    </main>
+    <UiContainer as={'main'} className={cn('flex-center')}>
+      index page
+    </UiContainer>
   );
 }
