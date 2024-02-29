6a1e12caa7a06816fa5458bfc7a693fb14469fc4
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-02-29 14:32:51 +0300

    ⬆️ change paddings

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -3,11 +3,12 @@ import { FC } from 'react';
 import UiContainer from '@/src/components/ui/UiContainer/UiContainer';
 
 import type { HeaderProps } from './Header.props';
+import cn from 'classnames';
 
 const Header: FC<HeaderProps> = () => {
   return (
     <header>
-      <UiContainer>Header</UiContainer>
+      <UiContainer className={cn('')}>Header</UiContainer>
     </header>
   );
 };
