a4720deeb3c20a14a254d183c3737d7aebf7f331
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-02-29 14:30:15 +0300

    🔁 create rotating animation

--- src/components/ui/Animation/Animation.tsx
+++ src/components/ui/Animation/Animation.tsx
@@ -3,15 +3,26 @@
 import type { AnimationProps } from './Animation.props';
 import { ShipWheel } from 'lucide-react';
 import { VariableFC } from '@xenopomp/advanced-types';
+import { motion } from 'framer-motion';
 
 const Animation: VariableFC<typeof ShipWheel, AnimationProps> = ({
   size = 150,
   ...props
 }) => {
   return (
-    <div aria-hidden>
+    <motion.div
+      aria-hidden
+      animate={{
+        rotate: [0, 360],
+      }}
+      transition={{
+        duration: 2.5,
+        ease: 'linear',
+        repeat: Infinity,
+      }}
+    >
       <ShipWheel size={size} {...props} />
-    </div>
+    </motion.div>
   );
 };
 
